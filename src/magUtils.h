c83c089ba494dfbe1574de70b199e81eb95dbddc
Brian J Smith <brian-j-smith@uiowa.edu>
2010-08-13 00:00:00 +0000

    version 0.2.1-2

--- src/magUtils.h
+++ src/magUtils.h
@@ -1,9 +1,24 @@
-/* 
+/********************************************************************************
  * File:   magUtils.h
- * Author: bjsmith
+ * Author: Brian J Smith <brian-j-smith@uiowa.edu>
  *
  * Created on June 21, 2010, 9:19 AM
- */
+ *
+ * This file is part of the magma R package.
+ *
+ * magma is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * magma is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with magma.  If not, see <http://www.gnu.org/licenses/>.
+ ********************************************************************************/
 
 #ifndef _MAGUTILS_H
 #define	_MAGUTILS_H
